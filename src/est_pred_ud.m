a47eb9370fcffae1fa92bbc5e1de17c8d84f54b0
Kurt Motekew <motekew@verizon.net>
2016-12-14 20:41:59 -0500

    implemented recursive form of U-D covariance propagation, finally

--- src/est_pred_ud.m
+++ src/est_pred_ud.m
@@ -1,7 +1,11 @@
-function [x, U, D] = est_pred_ud(x, U, D, Phi, Q, G)
-% EST_PRE_UD Updates a prior estimate's U-D covariance given a state transition
-% matrix and process noise matrix.  The decomposed form is such that
-% P = UDU' where U is a unit upper triangular matrix and D is a diagonal matrix.
+function [xbar, Ubar, Dbar] = est_pred_ud(xhat, Uhat, Dhat, Phi, Q, G)
+% EST_PRE_UD Updates a prior estimate's U-D covariance given a state
+% transition matrix and process noise matrix.  The decomposed form is
+% such that
+%   P = UDU'
+% where U is a unit upper triangular matrix and D is a diagonal matrix.
+% Note that the process noise matrix is diagonal.  Consult Bierman,
+% p. 125, w.r.t. whitening a non-diagonal Q matrix.
 %
 %-----------------------------------------------------------------------
 % Copyright 2016 Kurt Motekew
@@ -12,38 +16,57 @@ function [x, U, D] = est_pred_ud(x, U, D, Phi, Q, G)
 %-----------------------------------------------------------------------
 %
 % Inputs:
-%   x         A priori estimate, [nx1]
-%   U         A priori U matrix, [nxn]
-%   D         A priori D matrix, [nxn]
+%   xhat      A priori estimate, [nx1]
+%   Uhat      A priori U matrix, [nxn]
+%   Dhat      A priori D matrix, [nxn]
 %   Phi       State transition matrix, [nxn]
-%   Q         Process noise matrix, Diagonal of square Nw matrix, [1xNw] 
-%   G         Partials of estimate w.r.t. process noise, dx/dq, [nxNw]
+%   Q         Process noise matrix, DIAGONAL of square mxm matrix, [1xm] 
+%   G         Partials of estimate w.r.t. process noise, dx/dq, [nxm]
 %
 % Return:
-%   x   Updated estimated, [Nx1]
-%   U   Updated U, [NxN]
-%   D   Updated D, [NxN]
+%   xbar   Updated estimated, [nx1]
+%   Ubar   Updated U, [nxn]
+%   Dbar   Updated D, [nxn]
 %
-% Kurt Motekew   2016/11/22
+% Kurt Motekew   2016/12/14
 %
 %
 % Ref:  G. J. Bierman, Factorization Methods for
 %       Discrete Sequential Estimation, Dover Publications, Inc.,
-%       Mineola, NY, 1977, pp. 132-133
+%       Mineola, NY, 1977
+%       
+%       Byron D. Tapley, Bob E. Schutz, George H. Born, Statistical
+%       Orbit Determination, Elsevier Academic Press, 2004.
+%         The algorithm from page 345 was invaluable in deciphering
+%         a form that could be represented in terms of matrix algebra.
 %
 
-    % Linear state propagation
-  x = Phi*x;
-
-    % Cheat for now - need to implement recursive form
-  P = U*D*U';
-  P = Phi*P*Phi' + G*diag(Q)*G';
-  [U, D] = mth_udut2(P);
+  xbar = Phi*xhat;
 
     % Get matrix sizes for looping
-%  n = size(Phi,1);
-%  Nw = size(Q,2); 
-%  N = n + Nw;
+  n = size(Uhat,1);                         % Number of solve for
+  m = size(Q,2);                            % Number of consider parameters
+  N = n + m;
 
-%  v = zeros(1,n);
-  
+  A = [ Phi*Uhat G]';                       % [Nxn], a_j = A(:,j)
+  Dtmp((n+1):N,(n+1):N) = diag(Q);          % [NxN]
+  Dtmp(1:n,1:n) = Dhat;
+
+    % Could reuse Uhat and Dhat
+  Ubar = eye(n);
+  Dbar = zeros(n);
+  for ii = n:-1:1
+    c = Dtmp*A(:,ii);
+    Dbar(ii,ii) = A(:,ii)'*c;
+    d = c/Dbar(ii,ii);
+    for jj = 1:n
+      Ubar(jj,ii) = A(:,jj)'*d;
+      A(:,jj) = A(:,jj) - Ubar(jj,ii)*A(:,ii);
+    end
+  end
+
+    % Cheating method - but good for verification
+    % This is what we are doing in a more recursive manner
+%  P = U*D*U';
+%  P = Phi*P*Phi' + G*diag(Q)*G';
+%  [U, D] = mth_udut2(P);
